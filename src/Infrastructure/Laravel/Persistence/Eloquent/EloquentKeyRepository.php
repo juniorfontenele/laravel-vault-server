fee4b9167a1658cc88e820e1bfe8da75ee39663a
Junior Fontenele <github@juniorfontenele.com.br>
2025-04-30 11:24:48 -0300

    wip: EloquentKeyRepository

---
+++ src/Infrastructure/Laravel/Persistence/Eloquent/EloquentKeyRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace JuniorFontenele\LaravelVaultServer\Infrastructure\Laravel\Persistence\Eloquent;
+
+use JuniorFontenele\LaravelVaultServer\Domains\Vault\Key\Contracts\KeyRepositoryInterface;
+
+class EloquentKeyRepository implements KeyRepositoryInterface
+{
+}
