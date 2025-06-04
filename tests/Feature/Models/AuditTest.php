d1bfe157ca7b09fb09e246580e44a4548435ef8e
Junior Fontenele <github@juniorfontenele.com.br>
2025-06-04 11:11:17 -0300

    Refactor AuditTest to improve readability and structure of test cases

--- tests/Feature/Models/AuditTest.php
+++ tests/Feature/Models/AuditTest.php
@@ -13,17 +13,39 @@ beforeEach(function () {
     Client::query()->delete();
 });
 
-it('creates audit records on model events', function () {
-    $client = Client::factory()->create();
+describe('AuditModel', function () {
+    it('creates audit records on model events', function () {
+        $client = Client::factory()->create();
 
-    expect(Audit::count())->toBe(1)
-        ->and(Audit::first()->action)->toBe(AuditAction::CREATE)
-        ->and(Audit::first()->auditable_type)->toBe(Client::class)
-        ->and(Audit::first()->auditable_id)->toBe($client->id);
+        expect(Audit::count())->toBe(1)
+            ->and(Audit::first()->action)->toBe(AuditAction::CREATE)
+            ->and(Audit::first()->auditable_type)->toBe(Client::class)
+            ->and(Audit::first()->auditable_id)->toBe($client->id);
 
-    $retrieved = Client::find($client->id);
-    $retrieved->update(['description' => 'changed']);
-    $retrieved->delete();
+        $retrieved = Client::find($client->id);
+        $retrieved->update(['description' => 'changed']);
+        $retrieved->delete();
 
-    expect(Audit::count())->toBe(4);
+        expect(Audit::count())->toBe(4);
+    });
+
+    it('cannot delete audit records', function () {
+        $client = Client::factory()->create();
+        $audit = Audit::first();
+
+        expect(fn () => $audit->delete())->toThrow(RuntimeException::class, 'Audit records cannot be deleted.');
+        expect(Audit::count())->toBe(1);
+
+        expect(fn () => $audit->forceDelete())->toThrow(RuntimeException::class, 'Audit records cannot be deleted.');
+        expect(Audit::count())->toBe(1);
+    });
+
+    it('cannot update audit records', function () {
+        $client = Client::factory()->create();
+        $audit = Audit::first();
+
+        expect(fn () => $audit->update(['action' => AuditAction::UPDATE]))->toThrow(RuntimeException::class, 'Audit records cannot be updated.');
+        expect(Audit::count())->toBe(1);
+        expect(Audit::first()->action)->toBe(AuditAction::CREATE);
+    });
 });
